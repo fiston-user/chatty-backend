078aa839f4bac0b00f0ccd08a311bb6d88ab21a5
PlatnumMan <fistonrwanda@gmail.com>
2023-09-29 18:37:31 +0200

    feat: implemented video feature

--- src/features/post/controllers/update-post.ts
+++ src/features/post/controllers/update-post.ts
@@ -1,8 +1,8 @@
 import { joiValidation } from '@global/decorators/joi-validation.decorators';
-import { uploads } from '@global/helpers/cloudinary-upload';
+import { uploads, videoUpload } from '@global/helpers/cloudinary-upload';
 import { BadRequestError } from '@global/helpers/error-handler';
 import { IPostDocument } from '@post/interfaces/post.interface';
-import { postSchema, postWithImageSchema } from '@post/schemes/post.schemes';
+import { postSchema, postWithImageSchema, postWithVideoSchema } from '@post/schemes/post.schemes';
 import { imageQueue } from '@service/queues/image.queue';
 import { postQueue } from '@service/queues/post.queue';
 import { PostCache } from '@service/redis/post.cache';
@@ -16,7 +16,7 @@ const postCache: PostCache = new PostCache();
 export class Update {
   @joiValidation(postSchema)
   public async post(req: Request, res: Response): Promise<void> {
-    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture } = req.body;
+    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture, videoId, videoVersion } = req.body;
     const { postId } = req.params;
     const updatedPost: IPostDocument = {
       post,
@@ -26,7 +26,9 @@ export class Update {
       gifUrl,
       profilePicture,
       imgVersion,
-      imgId
+      imgId,
+      videoId,
+      videoVersion
     } as IPostDocument;
 
     const postUpdated: IPostDocument = await postCache.updatePostInCache(postId, updatedPost);
@@ -39,9 +41,9 @@ export class Update {
   public async postWithImage(req: Request, res: Response): Promise<void> {
     const { imgId, imgVersion } = req.body;
     if (imgId && imgVersion) {
-      Update.prototype.updatePostWithImage(req);
+      Update.prototype.updatePost(req);
     } else {
-      const result: UploadApiResponse = await Update.prototype.addImageToExistingPost(req);
+      const result: UploadApiResponse = await Update.prototype.addFileToExistingPost(req);
       if (!result?.public_id) {
         throw new BadRequestError(result.message);
       }
@@ -50,8 +52,23 @@ export class Update {
     res.status(HTTP_STATUS.OK).json({ message: 'Post with image updated successfully' });
   }
 
-  private async updatePostWithImage(req: Request): Promise<void> {
-    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture } = req.body;
+  @joiValidation(postWithVideoSchema)
+  public async postWithVideo(req: Request, res: Response): Promise<void> {
+    const { videoId, videoVersion } = req.body;
+    if (videoId && videoVersion) {
+      Update.prototype.updatePost(req);
+    } else {
+      const result: UploadApiResponse = await Update.prototype.addFileToExistingPost(req);
+      if (!result?.public_id) {
+        throw new BadRequestError(result.message);
+      }
+    }
+
+    res.status(HTTP_STATUS.OK).json({ message: 'Post with video updated successfully' });
+  }
+
+  private async updatePost(req: Request): Promise<void> {
+    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture, videoId, videoVersion } = req.body;
     const { postId } = req.params;
     const updatedPost: IPostDocument = {
       post,
@@ -60,8 +77,10 @@ export class Update {
       feelings,
       gifUrl,
       profilePicture,
-      imgVersion,
-      imgId
+      imgVersion: imgVersion ? imgVersion : '',
+      imgId: imgId ? imgId : '',
+      videoId: videoId ? videoId : '',
+      videoVersion: videoVersion ? videoVersion : ''
     } as IPostDocument;
 
     const postUpdated: IPostDocument = await postCache.updatePostInCache(postId, updatedPost);
@@ -69,10 +88,12 @@ export class Update {
     postQueue.addPostJob('updatePostInDB', { key: postId, value: postUpdated });
   }
 
-  private async addImageToExistingPost(req: Request): Promise<UploadApiResponse> {
-    const { post, bgColor, feelings, privacy, gifUrl, profilePicture, image } = req.body;
+  private async addFileToExistingPost(req: Request): Promise<UploadApiResponse> {
+    const { post, bgColor, feelings, privacy, gifUrl, profilePicture, image, video } = req.body;
     const { postId } = req.params;
-    const result: UploadApiResponse = (await uploads(image)) as UploadApiResponse;
+    const result: UploadApiResponse = image
+      ? ((await uploads(image)) as UploadApiResponse)
+      : ((await videoUpload(video)) as UploadApiResponse);
     if (!result?.public_id) {
       return result;
     }
@@ -83,18 +104,22 @@ export class Update {
       feelings,
       gifUrl,
       profilePicture,
-      imgId: result.public_id,
-      imgVersion: result.version.toString()
+      imgId: image ? result.public_id : '',
+      imgVersion: image ? result.version.toString() : '',
+      videoId: video ? result.public_id : '',
+      videoVersion: video ? result.version.toString() : ''
     } as IPostDocument;
 
     const postUpdated: IPostDocument = await postCache.updatePostInCache(postId, updatedPost);
     socketIOPostObject.emit('update post', postUpdated, 'posts');
     postQueue.addPostJob('updatePostInDB', { key: postId, value: postUpdated });
-    imageQueue.addImageJob('addImageToDB', {
-      key: req.currentUser!.userId,
-      imgId: result.public_id,
-      imgVersion: result.version.toString()
-    });
+    if (image) {
+      imageQueue.addImageJob('addImageToDB', {
+        key: req.currentUser!.userId,
+        imgId: result.public_id,
+        imgVersion: result.version.toString()
+      });
+    }
     return result;
   }
 }
--- src/shared/services/redis/post.cache.ts
+++ src/shared/services/redis/post.cache.ts
@@ -33,6 +33,8 @@ export class PostCache extends BaseCache {
       commentsCount,
       imgVersion,
       imgId,
+      videoId,
+      videoVersion,
       reactions,
       createdAt
     } = createdPost;
@@ -53,6 +55,8 @@ export class PostCache extends BaseCache {
       reactions: JSON.stringify(reactions),
       imgVersion: `${imgVersion}`,
       imgId: `${imgId}`,
+      videoId: `${videoId}`,
+      videoVersion: `${videoVersion}`,
       createdAt: `${createdAt}`
     };
 
@@ -130,6 +134,34 @@ export class PostCache extends BaseCache {
     }
   }
 
+  public async getPostsWithVideoFromCache(key: string, start: number, end: number): Promise<IPostDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const reply: string[] = await this.client.ZRANGE(key, start, end, { REV: true });
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+      for (const value of reply) {
+        multi.HGETALL(`posts:${value}`);
+      }
+      const replies: PostCacheMultiType = (await multi.exec()) as PostCacheMultiType;
+      const postWithVideos: IPostDocument[] = [];
+      for (const post of replies as IPostDocument[]) {
+        if (post.videoId && post.videoVersion) {
+          post.createdAt = new Date(Helpers.parseJson(`${post.createdAt}`)) as Date;
+          post.commentsCount = Helpers.parseJson(`${post.commentsCount}`) as number;
+          post.reactions = Helpers.parseJson(`${post.reactions}`) as IReactions;
+          postWithVideos.push(post);
+        }
+      }
+
+      return postWithVideos;
+    } catch (error) {
+      throw new InternalServerError('Server error. Try again.');
+    }
+  }
+
   public async getTotalPostsInCache(): Promise<number> {
     try {
       if (!this.client.isOpen) {
@@ -201,7 +233,7 @@ export class PostCache extends BaseCache {
   }
 
   public async updatePostInCache(key: string, updatedPost: IPostDocument): Promise<IPostDocument> {
-    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture } = updatedPost;
+    const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, videoId, videoVersion, profilePicture } = updatedPost;
     const dataToSave = {
       post: `${post}`,
       bgColor: `${bgColor}`,
@@ -210,7 +242,9 @@ export class PostCache extends BaseCache {
       gifUrl: `${gifUrl}`,
       profilePicture: `${profilePicture}`,
       imgVersion: `${imgVersion}`,
-      imgId: `${imgId}`
+      imgId: `${imgId}`,
+      videoId: `${videoId}`,
+      videoVersion: `${videoVersion}`
     };
 
     try {
