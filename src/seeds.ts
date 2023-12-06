68b8b938a32f3975905cfd77bd652c265056d82c
PlatnumMan <fistonrwanda@gmail.com>
2023-12-06 21:07:36 +0100

    SEED

--- src/seeds.ts
+++ src/seeds.ts
@@ -1,14 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { faker } from '@faker-js/faker';
 import axios from 'axios';
 import { createCanvas } from 'canvas';
 import dotenv from 'dotenv';
-import { floor, random } from 'lodash';
+import { random, uniq } from 'lodash';
 
 dotenv.config({});
 
 function avatarColor(): string {
-  const colors: string[] = [
+  const colors = uniq([
     '#FFEB3B',
     '#FFC107',
     '#FF9800',
@@ -23,61 +22,9 @@ function avatarColor(): string {
     '#009688',
     '#4CAF50',
     '#8BC34A',
-    '#CDDC39',
-    '#FFEB3B',
-    '#FFC107',
-    '#FF9800',
-    '#FF5722',
-    '#795548',
-    '#9E9E9E',
-    '#607D8B',
-    '#3F51B5',
-    '#2196F3',
-    '#03A9F4',
-    '#00BCD4',
-    '#009688',
-    '#4CAF50',
-    '#4CAF50',
-    '#8BC34A',
-    '#CDDC39',
-    '#FFEB3B',
-    '#FFC107',
-    '#FF9800',
-    '#FF5722',
-    '#795548',
-    '#9E9E9E',
-    '#607D8B',
-    '#3F51B5',
-    '#2196F3',
-    '#03A9F4',
-    '#00BCD4',
-    '#009688',
-    '#4CAF50',
-    '#8BC34A',
-    '#CDDC39',
-    '#FFEB3B',
-    '#FFC107',
-    '#FF9800',
-    '#FF5722',
-    '#795548',
-    '#9E9E9E',
-    '#607D8B',
-    '#3F51B5',
-    '#2196F3',
-    '#03A9F4',
-    '#00BCD4',
-    '#009688',
-    '#4CAF50',
-    '#8BC34A',
-    '#CDDC39',
-    '#FFEB3B',
-    '#FFC107',
-    '#FF9800',
-    '#FF5722',
-    '#795548',
-    '#9E9E9E'
-  ];
-  return colors[floor(random(0.9) * colors.length)];
+    '#CDDC39'
+  ]);
+  return colors[Math.floor(random(colors.length))];
 }
 
 function generateAvatar(text: string, backgroundColor: string, foregroundColor = 'white') {
@@ -97,10 +44,9 @@ function generateAvatar(text: string, backgroundColor: string, foregroundColor =
 }
 
 async function seedUserData(count: number): Promise<void> {
-  let i = 0;
-  try {
-    for (i = 0; i < count; i++) {
-      const username: string = faker.unique(faker.word.adjective, [8]);
+  for (let i = 0; i < count; i++) {
+    try {
+      const username = faker.unique(faker.word.adjective, [8]);
       const color = avatarColor();
       const avatar = generateAvatar(username.charAt(0).toUpperCase(), color);
 
@@ -111,11 +57,12 @@ async function seedUserData(count: number): Promise<void> {
         avatarColor: color,
         avatarImage: avatar
       };
-      console.log(`***ADDING USER TO DATABASE*** - ${i - 1} of ${count} - ${username}`);
+
+      console.log(`***ADDING USER TO DATABASE*** - ${i} of ${count} - ${username}`);
       await axios.post(`${process.env.API_URL}/signup`, body);
+    } catch (error) {
+      console.error('Error adding user:', error);
     }
-  } catch (error: any) {
-    console.log(error?.response?.data);
   }
 }
 
