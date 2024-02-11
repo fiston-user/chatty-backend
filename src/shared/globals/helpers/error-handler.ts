73856a795ab656ae5f31b69d99580dc67a34aa1d
fiston-user <fistonrwanda@gmail.com>
2024-02-11 09:45:37 +0100

    setup project

---
+++ src/shared/globals/helpers/error-handler.ts
@@ -0,0 +1,94 @@
+import HTTP_STATUS from 'http-status-codes';
+
+export interface IErrorResponse {
+  message: string;
+  statusCode: number;
+  status: string;
+  serializeErrors(): IError;
+}
+
+export interface IError {
+  message: string;
+  statusCode: number;
+  status: string;
+}
+
+export abstract class CustomError extends Error {
+  abstract statusCode: number;
+  abstract status: string;
+
+  constructor(message: string) {
+    super(message);
+  }
+
+  serializeErrors(): IError {
+    return {
+      message: this.message,
+      statusCode: this.statusCode,
+      status: this.status
+    };
+  }
+}
+
+export class JoiRequestValidationError extends CustomError {
+  statusCode = HTTP_STATUS.BAD_REQUEST;
+  status = 'error';
+
+  constructor(public message: string) {
+    super(message);
+  }
+}
+
+export class BadRequestError extends CustomError {
+  statusCode = HTTP_STATUS.BAD_REQUEST;
+  status = 'error';
+
+  constructor(public message: string) {
+    super(message);
+  }
+}
+
+export class NotFoundError extends CustomError {
+  statusCode = HTTP_STATUS.NOT_FOUND;
+  status = 'error';
+
+  constructor(public message: string) {
+    super(message);
+  }
+}
+
+export class UnauthorizedError extends CustomError {
+  statusCode = HTTP_STATUS.UNAUTHORIZED;
+  status = 'error';
+
+  constructor(public message: string) {
+    super(message);
+  }
+}
+
+export class ForbiddenError extends CustomError {
+  statusCode = HTTP_STATUS.FORBIDDEN;
+  status = 'error';
+
+  constructor(public message: string) {
+    super(message);
+  }
+}
+
+export class FileTooLargeError extends CustomError {
+  statusCode = HTTP_STATUS.REQUEST_TOO_LONG;
+  status = 'error';
+
+  constructor(public message: string) {
+    super(message);
+  }
+}
+
+export class ServerError extends CustomError {
+  statusCode = HTTP_STATUS.SERVICE_UNAVAILABLE;
+  status = 'error';
+
+  constructor(public message: string) {
+    super(message);
+  }
+}
