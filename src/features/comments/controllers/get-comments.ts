a684ba38e92fe951ee4a77937371a1b53d484d4c
fiston-user <fistonrwanda@gmail.com>
2024-02-14 01:39:19 +0100

    feature: Implemented comment with unit tests

---
+++ src/features/comments/controllers/get-comments.ts
@@ -0,0 +1,42 @@
+import { ICommentDocument, ICommentNameList } from '@comment/interfaces/comment.interface';
+import { commentService } from '@service/db/comment.service';
+import { CommentCache } from '@service/redis/comment.cache';
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import mongoose from 'mongoose';
+
+const commentCache: CommentCache = new CommentCache();
+
+export class Get {
+  public async comments(req: Request, res: Response): Promise<void> {
+    const { postId } = req.params;
+    const cachedComments: ICommentDocument[] = await commentCache.getCommentsFromCache(postId);
+    const comments: ICommentDocument[] = cachedComments.length
+      ? cachedComments
+      : await commentService.getPostComments({ postId: new mongoose.Types.ObjectId(postId) }, { createdAt: -1 });
+
+    res.status(HTTP_STATUS.OK).json({ message: 'Post comments retrieved successfully', comments });
+  }
+
+  public async commentNamesFromCache(req: Request, res: Response): Promise<void> {
+    const { postId } = req.params;
+    const cachedCommentsNames: ICommentNameList[] = await commentCache.getCommentsNamesFromCache(postId);
+    const commentsNames: ICommentNameList[] = cachedCommentsNames.length
+      ? cachedCommentsNames
+      : await commentService.getPostCommentNames({ postId: new mongoose.Types.ObjectId(postId) }, { createdAt: -1 });
+
+    res
+      .status(HTTP_STATUS.OK)
+      .json({ message: 'Post comments names retrieved successfully', comments: commentsNames.length ? commentsNames[0] : [] });
+  }
+
+  public async singleComment(req: Request, res: Response): Promise<void> {
+    const { postId, commentId } = req.params;
+    const cachedComments: ICommentDocument[] = await commentCache.getSingleCommentFromCache(postId, commentId);
+    const comments: ICommentDocument[] = cachedComments.length
+      ? cachedComments
+      : await commentService.getPostComments({ _id: new mongoose.Types.ObjectId(commentId) }, { createdAt: -1 });
+
+    res.status(HTTP_STATUS.OK).json({ message: 'Single comment retrieved successfully', comments: comments[0] });
+  }
+}
