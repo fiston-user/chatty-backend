0613dbd0ecd46e681a1521b344a597f150f83933
fiston-user <fistonrwanda@gmail.com>
2024-02-19 03:35:31 +0100

    Add installation of AWS CLI and update-env-file.sh script

--- deployment/userdata/update-env-file.sh
+++ deployment/userdata/update-env-file.sh
@@ -13,6 +13,11 @@ if [ $(program_is_installed zip) == 0 ]; then
    apk add zip
 fi
 
+if [ $(program_is_installed aws) == 0 ]; then
+   apk update
+   apk add --no-cache aws-cli
+fi
+
 aws s3 sync s3://chat-app-env-files/develop .
 unzip env-file.zip
 cp .env.develop .env
