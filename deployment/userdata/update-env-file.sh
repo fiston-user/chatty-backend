bd8f4ae379a60f2e96c4d4d5274f8fa57ca6cdd6
fiston-user <fistonrwanda@gmail.com>
2024-02-19 01:35:32 +0100

    Add AWS CLI setup and check for zip program installation

--- deployment/userdata/update-env-file.sh
+++ deployment/userdata/update-env-file.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+function program_is_installed {
+  local return_=1
+
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  echo "$return_"
+}
+
+
+if [ $(program_is_installed zip) == 0 ]; then
+   apk update
+   apk add zip
+fi
+
 aws s3 sync s3://chat-app-env-files/production .
 unzip env-file.zip
 cp .env.production .env
